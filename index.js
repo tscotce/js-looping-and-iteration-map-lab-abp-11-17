7786afd000caba9eb406a4e7d30e81ad61c93b00
Tatyana Scotce <tscotce@gmail.com>
2017-12-18 23:27:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function nameToAttributes (drivers) {
 
 function attributesToPhrase (drivers) {
   return drivers.map (function (driver) {
-    return `${driver.name} is from ${driver.hometown}  
-  })
+    return `${driver.name} is from ${driver.hometown}`;  
+  });
 }
