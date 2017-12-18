dd673b276c18e46bb1941f3e0a136780a58a4f81
Tatyana Scotce <tscotce@gmail.com>
2017-12-18 23:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function lowerCaseDrivers (drivers) {
   const newNames = [];
     for (const element of array) {
-    newNames.push (drivers (drivers.toLowerCase))
+    newNames.push (drivers (drivers.toLowerCase));
     }
   }
