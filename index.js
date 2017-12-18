c86cfa4dca6ffae2ab48e50ca977b8a44027096d
Tatyana Scotce <tscotce@gmail.com>
2017-12-18 23:25:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ function lowerCaseDrivers (drivers) {
 function nameToAttributes (drivers) {
   return drivers.map (function (driver) {
     const driverFirstName = driver.split(' ')[0];
-    const driverLastName = driver.split(' ')[0];
+    const driverLastName = driver.split(' ')[1];
     return {firstName: driverFirstName, lastName: driverLastName};
   });
 }
 
 
+}
